3f656456eebca38a0f6d5504b0d22438c33070b8
OreOluwaDaniel <=>
2019-12-17 19:58:23 +0100

    caching issue tried out

--- includes/head.php
+++ includes/head.php
@@ -14,27 +14,27 @@
         <title><?php echo $pageTitle ; ?> - Dre Files</title>
 
         <!-- Plugins css-->
-        <link href="<?= $root ?>assets/css/treestyle.css" rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>plugins/bootstrap-tagsinput/css/bootstrap-tagsinput.css" rel="stylesheet" />
-        <link href="<?= $root ?>plugins/multiselect/css/multi-select.css"  rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>plugins/bootstrap-select/css/bootstrap-select.min.css" rel="stylesheet" />
-        <link href="<?= $root ?>plugins/bootstrap-touchspin/css/jquery.bootstrap-touchspin.min.css" rel="stylesheet" />
+        <link href="<?= $root ?>assets/css/treestyle.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>plugins/bootstrap-tagsinput/css/bootstrap-tagsinput.css?v=1.22" rel="stylesheet" />
+        <link href="<?= $root ?>plugins/multiselect/css/multi-select.css?v=1.22"  rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>plugins/select2/css/select2.min.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>plugins/bootstrap-select/css/bootstrap-select.min.css?v=1.22" rel="stylesheet" />
+        <link href="<?= $root ?>plugins/bootstrap-touchspin/css/jquery.bootstrap-touchspin.min.css?v=1.22" rel="stylesheet" />
 
         <!-- App css -->
-        <link href="<?= $root ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>assets/css/core.css" rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>assets/css/components.css" rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>assets/css/icons.css" rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>assets/css/pages.css" rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>assets/css/menu.css" rel="stylesheet" type="text/css" />
-        <link href="<?= $root ?>assets/css/responsive.css" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="<?= $root ?>plugins/switchery/switchery.min.css">
+        <link href="<?= $root ?>assets/css/bootstrap.min.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>assets/css/core.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>assets/css/components.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>assets/css/icons.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>assets/css/pages.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>assets/css/menu.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link href="<?= $root ?>assets/css/responsive.css?v=1.22" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="<?= $root ?>plugins/switchery/switchery.min.css?v=1.22">
 
         
-        <link href="<?= $root ?>../plugins/datatables/dataTables.colVis.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $root ?>../plugins/datatables/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="<?= $root ?>../plugins/datatables/fixedColumns.dataTables.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?= $root ?>../plugins/datatables/dataTables.colVis.css?v=1.22" rel="stylesheet" type="text/css"/>
+        <link href="<?= $root ?>../plugins/datatables/dataTables.bootstrap.min.css?v=1.22" rel="stylesheet" type="text/css"/>
+        <link href="<?= $root ?>../plugins/datatables/fixedColumns.dataTables.min.css?v=1.22" rel="stylesheet" type="text/css"/>
 
 
         <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
--- includes/scripts.php
+++ includes/scripts.php
@@ -2,57 +2,57 @@
     $root = 'http://'.$_SERVER['SERVER_NAME'].'/' ;
 ?>
 <!-- jQuery  -->
-        <script src="<?= $root ?>assets/js/jquery.min.js"></script>
-        <script src="<?= $root ?>assets/js/bootstrap.min.js"></script>
-        <script src="<?= $root ?>assets/js/detect.js"></script>
-        <script src="<?= $root ?>assets/js/fastclick.js"></script>
-        <script src="<?= $root ?>assets/js/jquery.blockUI.js"></script>
-        <script src="<?= $root ?>assets/js/waves.js"></script>
-        <script src="<?= $root ?>assets/js/jquery.slimscroll.js"></script>
-        <script src="<?= $root ?>assets/js/jquery.form.min.js"></script>
-        <script src="<?= $root ?>assets/js/jquery.scrollTo.min.js"></script>
-        <script src="<?= $root ?>plugins/switchery/switchery.min.js"></script>
+        <script src="<?= $root ?>assets/js/jquery.min.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/bootstrap.min.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/detect.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/fastclick.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/jquery.blockUI.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/waves.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/jquery.slimscroll.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/jquery.form.min.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/jquery.scrollTo.min.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/switchery/switchery.min.js?v=1.2"></script>
 
-        <script src="<?= $root ?>plugins/bootstrap-tagsinput/js/bootstrap-tagsinput.min.js"></script>
-        <script type="text/javascript" src="<?= $root ?>plugins/multiselect/js/jquery.multi-select.js"></script>
-        <script type="text/javascript" src="<?= $root ?>plugins/jquery-quicksearch/jquery.quicksearch.js"></script>
-        <script src="<?= $root ?>plugins/select2/js/select2.min.js" type="text/javascript"></script>
-        <script src="<?= $root ?>plugins/bootstrap-select/js/bootstrap-select.min.js" type="text/javascript"></script>
-        <script src="<?= $root ?>plugins/bootstrap-filestyle/js/bootstrap-filestyle.min.js" type="text/javascript"></script>
-        <script src="<?= $root ?>plugins/bootstrap-touchspin/js/jquery.bootstrap-touchspin.min.js" type="text/javascript"></script>
-        <script src="<?= $root ?>plugins/bootstrap-maxlength/bootstrap-maxlength.min.js" type="text/javascript"></script>
+        <script src="<?= $root ?>plugins/bootstrap-tagsinput/js/bootstrap-tagsinput.min.js?v=1.2"></script>
+        <script type="text/javascript" src="<?= $root ?>plugins/multiselect/js/jquery.multi-select.js?v=1.2"></script>
+        <script type="text/javascript" src="<?= $root ?>plugins/jquery-quicksearch/jquery.quicksearch.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/select2/js/select2.min.js?v=1.2" type="text/javascript"></script>
+        <script src="<?= $root ?>plugins/bootstrap-select/js/bootstrap-select.min.js?v=1.2" type="text/javascript"></script>
+        <script src="<?= $root ?>plugins/bootstrap-filestyle/js/bootstrap-filestyle.min.js?v=1.2" type="text/javascript"></script>
+        <script src="<?= $root ?>plugins/bootstrap-touchspin/js/jquery.bootstrap-touchspin.min.js?v=1.2" type="text/javascript"></script>
+        <script src="<?= $root ?>plugins/bootstrap-maxlength/bootstrap-maxlength.min.js?v=1.2" type="text/javascript"></script>
 
-        <script type="text/javascript" src="<?= $root ?>plugins/autocomplete/jquery.mockjax.js"></script>
-        <script type="text/javascript" src="<?= $root ?>plugins/autocomplete/jquery.autocomplete.min.js"></script>
-        <script type="text/javascript" src="<?= $root ?>plugins/autocomplete/countries.js"></script>
-        <!-- <script type="text/javascript" src="<?= $root ?>assets/pages/jquery.autocomplete.init.js"></script> -->
+        <script type="text/javascript" src="<?= $root ?>plugins/autocomplete/jquery.mockjax.js?v=1.2"></script>
+        <script type="text/javascript" src="<?= $root ?>plugins/autocomplete/jquery.autocomplete.min.js?v=1.2"></script>
+        <script type="text/javascript" src="<?= $root ?>plugins/autocomplete/countries.js?v=1.2"></script>
+        <!-- <script type="text/javascript" src="<?= $root ?>assets/pages/jquery.autocomplete.init.js?v=1.2"></script> -->
 
-        <script type="text/javascript" src="<?= $root ?>assets/pages/jquery.form-advanced.init.js"></script>
+        <script type="text/javascript" src="<?= $root ?>assets/pages/jquery.form-advanced.init.js?v=1.2"></script>
 
 
-        <script src="<?= $root ?>plugins/datatables/jquery.dataTables.min.js"></script>
-        <script src="<?= $root ?>plugins/datatables/dataTables.bootstrap.js"></script>
+        <script src="<?= $root ?>plugins/datatables/jquery.dataTables.min.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/datatables/dataTables.bootstrap.js?v=1.2"></script>
 
-        <script src="<?= $root ?>plugins/datatables/dataTables.buttons.min.js"></script>
-        <script src="<?= $root ?>plugins/datatables/dataTables.fixedHeader.min.js"></script>
-        <script src="<?= $root ?>plugins/datatables/dataTables.keyTable.min.js"></script>
-        <script src="<?= $root ?>plugins/datatables/dataTables.responsive.min.js"></script>
-        <script src="<?= $root ?>plugins/datatables/responsive.bootstrap.min.js"></script>
-        <script src="<?= $root ?>plugins/datatables/dataTables.scroller.min.js"></script>
-        <script src="<?= $root ?>plugins/datatables/dataTables.colVis.js"></script>x        
-        <script src="<?= $root ?>plugins/datatables/dataTables.fixedColumns.min.js"></script>
+        <script src="<?= $root ?>plugins/datatables/dataTables.buttons.min.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/datatables/dataTables.fixedHeader.min.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/datatables/dataTables.keyTable.min.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/datatables/dataTables.responsive.min.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/datatables/responsive.bootstrap.min.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/datatables/dataTables.scroller.min.js?v=1.2"></script>
+        <script src="<?= $root ?>plugins/datatables/dataTables.colVis.js?v=1.2"></script>x        
+        <script src="<?= $root ?>plugins/datatables/dataTables.fixedColumns.min.js?v=1.2"></script>
 
         <!-- init -->
-        <script src="<?= $root ?>assets/pages/jquery.datatables.init.js"></script>
+        <script src="<?= $root ?>assets/pages/jquery.datatables.init.js?v=1.2"></script>
 
         <!-- App js -->
-        <script src="<?= $root ?>assets/js/jstree.min.js"></script>
-        <script src="<?= $root ?>assets/js/jquery.tree.js"></script>
+        <script src="<?= $root ?>assets/js/jstree.min.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/jquery.tree.js?v=1.2"></script>
 
-        <script src="<?= $root ?>assets/js/jquery.core.js"></script>
-        <script src="<?= $root ?>assets/js/jquery.app.js"></script>
+        <script src="<?= $root ?>assets/js/jquery.core.js?v=1.2"></script>
+        <script src="<?= $root ?>assets/js/jquery.app.js?v=1.2"></script>
 
-        <script src="<?= $root ?>assets/js/custom.js"></script>
+        <script src="<?= $root ?>assets/js/custom.js?v=1.2"></script>
 
 
 
